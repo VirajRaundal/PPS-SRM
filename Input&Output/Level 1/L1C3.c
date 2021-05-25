9c1b9fb9e8831dcf5e9d4684e40415a2d4600521
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2021-05-25 13:03:59 +0530

    I/O Level 1
    Input-Output Level 1 to 10.

---
+++ Input&Output/Level 1/L1C3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    float var1,var2,res;
+    scanf("%f",var1);
+    scanf("%f",var2);
+    res=var1+var2;
+    printf("%.3lf\n",res);
+    return 0;
+}
---
+++ Input&Output/Level 1/L1C6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int iplno;
+    scanf("%d", &iplno);
+    print("%o\n", iplno);
+    print("%X\n", iplno);
+    return 0;
+} //Doesn't work in Elab cause the output is in capital letters ig.
