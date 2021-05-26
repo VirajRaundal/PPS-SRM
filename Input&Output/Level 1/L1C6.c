e22b322b74e35ef54f4d56e7c1db95339af35ecd
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2021-05-27 03:10:52 +0530

    Update L1C6.c

--- Input&Output/Level 1/L1C6.c
+++ Input&Output/Level 1/L1C6.c
@@ -4,6 +4,6 @@ int main(){
     int iplno;
     scanf("%d", &iplno);
     print("%o\n", iplno);
-    print("%X\n", iplno);
+    print("%x\n", iplno);
     return 0;
-} //Doesn't work in Elab cause the output is in capital letters ig.
+}
